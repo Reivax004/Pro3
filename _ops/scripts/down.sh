8c99c21d848e421b2875a2ac9b1e0d044c347f05
Xavier Alibert <xavier.alibert@etu-paris1.fr>
2025-04-14 16:45:29 +0200

    modiff

--- _ops/scripts/down.sh
+++ _ops/scripts/down.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash 
-set -evo pipefail
-docker compose -f ../docker-compose.yaml -p pro3 down -d --build
+set -euo pipefail
+docker compose -f ../docker-compose.yaml -p pro3 down
--- _ops/scripts/up.sh
+++ _ops/scripts/up.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash 
 set -euo pipefail
-source ../services/node/file/.env && docker compose -f ../docker-compose.yaml -p pro3 up --build
+docker compose -f ../docker-compose.yaml -p pro3 up -d --build
